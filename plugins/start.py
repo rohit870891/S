04a7ae129ecc7db28a084ae7dba1adfc68015a82
rohit870891 <koyeb77user@gmail.com>
2024-09-17 20:42:33 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -209,7 +209,20 @@ async def start_command(client: Client, message: Message):
                     [InlineKeyboardButton('BUY PREMIUM', callback_data='buy_prem')]
                 ]
             )
-            
+             await message.reply_text(
+                text=START_MSG.format(
+                    first=message.from_user.first_name,
+                    last=message.from_user.last_name,
+                    username=None if not message.from_user.username else '@' + message.from_user.username,
+                    mention=message.from_user.mention,
+                    id=message.from_user.id
+                ),
+                reply_markup=reply_markup,
+                disable_web_page_preview=True,
+                quote=True
+            )
+        except Exception as e:
+            print(e)
 
 #=====================================================================================##
 
