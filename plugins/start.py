1b6bdd23607a5708683af125106e13fb7c306336
rohit870891 <koyeb77user@gmail.com>
2024-09-17 20:32:18 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -198,7 +198,7 @@ async def start_command(client: Client, message: Message):
                     await asyncio.sleep(e.x)
                     snt_msg = await msg.copy(chat_id=message.from_user.id, caption=caption, parse_mode=ParseMode.HTML, reply_markup=reply_markup, protect_content=PROTECT_CONTENT)
                     snt_msgs.append(snt_msg)
-                                except:
+                except:
                     pass
             return
     else:
